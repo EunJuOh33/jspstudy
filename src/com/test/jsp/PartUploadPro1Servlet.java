7053e2443defc2020ef306aee7c77e878404e4e2
yn05 <yn05@LAPTOP-6JQC2MPS>
2020-05-11 12:46:44 +0900

    Part 인터페이스를 사용한 업로드 구현

---
+++ src/com/test/jsp/PartUploadPro1Servlet.java
@@ -0,0 +1,62 @@
+package com.test.jsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@WebServlet("/partUploadPro1Servlet")	// 첫 글자를 소문자로 바꿔준다.
+@MultipartConfig(
+		fileSizeThreshold = 0,
+		location = "C:/tomcat9/wtpwebapps/jspstudy/upload"
+)
+
+public class PartUploadPro1Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProc(request, response);
+	}
+	
+	protected void doProc(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String writer = request.getParameter("writer");
+		Part part = request.getPart("partFile1");
+		response.setContentType("text/html); charset=UTF-8");	// 한글설정
+		PrintWriter out = response.getWriter();
+		String contentDisposition = part.getHeader("content-disposition");
+		String uploadFileName = getUploadFileName(contentDisposition, request);	
+		part.write(uploadFileName);
+		out.println("작성자" + writer + "님이" + uploadFileName + "파일을 업로드 하였습니다.");
+	}
+	
+
+	// 사용 브라우저가 IE거나 Chrome인 경우
+	private String getUploadFileName(String contentDisposition, HttpServletRequest request, HttpServletResponse response) {
+		String agent = request.getHeader("Uswer-Agent");
+		boolean ieBrowser = (agent.indexOf("MSIE") > -1) || (agent.indexOf("Trident") > -1);
+		
+		if(ieBrowser) {
+			int lastPathSeparatorIndex = contentSplitStr[2].lastIndexOf("\\");
+			int lastQutosIndex = contentSplitr[2].lastIndexOf("\"");
+			uploadFileName = contentSplitStr[2].substring(lastPathSeparatorIndex + 1, lastQutosIndex);
+		} else {
+			int firstQutosIndex = contentSplitStr[2].indexOf("\\");
+			int lastQutosIndex = contentSplitStr[2].lastIndexOf("\"");
+			uploadFileName = contentSplitStr[2].substring(firstQutosIndex + 1, lastQutosIndex);
+		}
+
+		return uploadFileName;
+	}
+}
